139cd769cb21c9600295841179f39840d0976bec
Muhammad Owais Ansari <owais.ansaricr7@gmail.com>
2023-09-12 01:03:47 +0500

    folder added

---
+++ chart.js/src/app/mychart/mychart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mychart',
+  templateUrl: './mychart.component.html',
+  styleUrls: ['./mychart.component.css']
+})
+export class MychartComponent {
+
+}
